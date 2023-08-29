3841942feee53032ccef29f4a0b331147979944c
Martin <143501628+AlcDotCom@users.noreply.github.com>
2023-08-29 14:34:14 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using AventStack.ExtentReports.Model;
+using ExcelMonitor.Properties;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using Excel = Microsoft.Office.Interop.Excel;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace ExcelMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = Settings.Default.zalozka;
+            GetImage();
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = Convert.ToInt32(Settings.Default.frekvencia);
+            aTimer.Enabled = true;
+        }
+
+        void GetImage()
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+            }
+                Exception threadEx = null;
+                Thread staThread = new Thread(
+                    delegate ()
+                    {
+                        try
+                        {
+                            var a = new Application();
+                            Workbook w = a.Workbooks.Open(@Settings.Default.adresa.ToString(), false);
+                            Worksheet ws = w.Sheets[Settings.Default.zalozka];
+                            Range r = ws.Range[Settings.Default.bunky];
+                            try
+                            {
+                            Clipboard.Clear();
+                            r.CopyPicture(XlPictureAppearance.xlScreen, XlCopyPictureFormat.xlBitmap); 
+                            }
+                            catch (Exception)
+                            {
+                                Marshal.ReleaseComObject(r);
+                                Marshal.ReleaseComObject(ws);
+                                //w.Save();
+                                w.Close(false);
+                                Marshal.ReleaseComObject(w);
+                                a.Quit();
+                                Marshal.ReleaseComObject(a);
+
+                                GC.Collect();
+                                GC.WaitForPendingFinalizers();
+                                return; 
+                            }
+
+                            Bitmap image = new Bitmap(Clipboard.GetImage());
+                            pictureBox1.Image = image;
+
+                            Marshal.ReleaseComObject(r);
+                            Marshal.ReleaseComObject(ws);
+                            //w.Save();
+                            w.Close(false);
+                            Marshal.ReleaseComObject(w);
+                            a.Quit();
+                            Marshal.ReleaseComObject(a);
+
+                            GC.Collect();
+                            GC.WaitForPendingFinalizers();
+
+                        }
+                        catch (Exception ex)
+                        {
+                             threadEx = ex;
+                        }
+                    });
+                staThread.SetApartmentState(ApartmentState.STA);
+                staThread.Start();
+                staThread.Join();
+        }
+
+        void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            GetImage();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+    }
+}
---
+++ Nastavenie.cs
@@ -0,0 +1,95 @@
+﻿using ExcelMonitor.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelMonitor
+{
+    public partial class Nastavenie : Form
+    {
+        public Nastavenie()
+        {
+            InitializeComponent();
+            textBox1.Text = Settings.Default.adresa;
+            textBox2.Text = Settings.Default.zalozka;
+            textBox3.Text = Settings.Default.bunky;
+            textBox4.Text = Settings.Default.frekvencia;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Settings.Default.adresa = textBox1.Text;
+            Settings.Default.Save();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Settings.Default.zalozka = textBox2.Text;
+            Settings.Default.Save();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Settings.Default.bunky = textBox3.Text;
+            Settings.Default.Save();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int.Parse(textBox4.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Zadaná hodnota nie je číslovka!");
+                textBox4.Text = "60000";
+                textBox4.SelectAll();
+                return;
+            }
+            Settings.Default.frekvencia = textBox4.Text;
+            Settings.Default.Save();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Settings.Default.adresa == "")
+            {
+                MessageBox.Show("Vložte adresu súboru");
+                textBox1.Focus();
+                return;
+            }
+            else if (Settings.Default.zalozka == "")
+            {
+                MessageBox.Show("Vložte názov záložky");
+                textBox2.Focus();
+                return;
+            }
+            else if (Settings.Default.bunky == "")
+            {
+                MessageBox.Show("Vložte zobrazované bunky");
+                textBox3.Focus();
+                return;
+            }
+            else if (Settings.Default.frekvencia == "")
+            {
+                MessageBox.Show("Vložte frekvenciu obnovy");
+                textBox4.Focus();
+                return;
+            }
+            else
+            {
+                Form1 nextForm = new Form1();
+                Hide();
+                nextForm.ShowDialog();
+                Close();
+            }
+        }
+    }
+}
